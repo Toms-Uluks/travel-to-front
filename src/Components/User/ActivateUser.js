49e6d012e488d6c2c8471e0123f81369eca81a8c
Toms-Uluks <ulukst@gmail.com>
2020-01-06 15:02:02 +0100

    added some stiling to confirmation page

--- src/Components/User/ActivateUser.js
+++ src/Components/User/ActivateUser.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Axios from 'axios';
 
 class ActivateUser extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            user: {
+                message: "",
+                success: false
+            }
+        }
+    }
+    componentDidMount() {
+        var config = {
+            headers: {'Authorization': "Bearer " + this.props.match.params.userToken}
+          };
+          Axios.get("https://travel-to-api.herokuapp.com/api/user", config).then(res => {
+              console.log(res)
+            if(res.data.status === 'success') {
+                this.setState({
+                    message: res.data.data.name + ', your account has been activated',
+                    success: true
+                })
+            } else {
+                this.setState({
+                    message: 'Something went wrong, try again later!',
+                    success: false
+                })
+            }
+          })
+    
     }
     render() { 
         return (  
             <div>
-                Your user ({this.props.match.params.userToken}) has been activated ;)
-                <Link to="/">Start your journey</Link>
+                <div className="auth-message-block">{this.state.message}</div>
+                {this.state.success ? <Link to="/">Start your journey</Link> : null }
             </div>
         );
     }
